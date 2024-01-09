15eaede30cf097f8602fffedf794711da91fc5cb
Victor Polyakov <polyakov.victor21@gmail.com>
2024-01-09 22:12:01 +0200

    django/ added get method

--- todoai_Django/goals/urls.py
+++ todoai_Django/goals/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import TodoItemView
 
 app_name = 'goals'
 
 urlpatterns = [
+    path('items/', TodoItemView.as_view(), name='todoItem'),
 ]
--- todoai_Django/goals/views.py
+++ todoai_Django/goals/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import TodoItem
+from .serializer import TodoItemSerializer
 
-# Create your views here.
+
+class TodoItemView(APIView):
+    serializer_class = TodoItemSerializer  
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        queryset = self.get_queryset()
+        serializer = TodoItemSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_queryset(self):
+        return TodoItem.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
